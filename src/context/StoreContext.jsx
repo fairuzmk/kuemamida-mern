25e7d4ca4f2b821e805ec1a93126316a669cf1da
Fairuz_lenovo <fairuzdeveloper@gmail.com>
2025-07-07 14:44:49 +0700

    test url to render

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -10,7 +10,7 @@ const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
 
-    const url = "http://localhost:4000";
+    const url = "https://kuemamida-backend.onrender.com";
     const [token, setToken] = useState(() => {
         return localStorage.getItem("token") || "";
     });
